467d1c0115f79788c882a777ef9554c8e965a4a4
Vien Tran <vien.tran@notabasement.com>
2019-10-07 13:07:35 +0700

    Add Carthage support

---
+++ Project/BDKCollectionIndexView/BDKCollectionIndexView-umbrella.h
@@ -0,0 +1,18 @@
+//
+//  BDKCollectionIndexView.h
+//  BDKCollectionIndexView
+//
+//  Created by Vien Tran on 10/7/19.
+//  Copyright © 2019 Ben Kreeger. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for BDKCollectionIndexView.
+FOUNDATION_EXPORT double BDKCollectionIndexViewVersionNumber;
+
+//! Project version string for BDKCollectionIndexView.
+FOUNDATION_EXPORT const unsigned char BDKCollectionIndexViewVersionString[];
+
+#import "BDKCollectionIndexView.h"
+
